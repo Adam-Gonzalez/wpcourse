a6bcec61962f2525cf15f4dd0ddb46cfee3a0388
Adam Gonzalez <adam007g@yahoo.com>
2024-02-23 16:10:56 -0600

    updated metabox

--- page.php
+++ page.php
@@ -15,16 +15,17 @@ while (have_posts()) {
 
     <div class="container container--narrow page-section">
 
-    <?php if (wp_get_post_parent_id(get_the_ID())) { ?>
+    <?php 
+    $theParent=(wp_get_post_parent_id(get_the_ID()));
+    if ($theParent) { ?>
         <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?> </span>
-
-      </div>
-    <?php } ?>
- 
-
-    
+    <p>
+        <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" 
+        aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> 
+        <span class="metabox__main"><?php the_title(); ?> </span>
+    </p>
+</div>
+    <?php }
       
 <!--
       <div class="page-links">
